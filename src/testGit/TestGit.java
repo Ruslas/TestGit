bad881ada2769004d01433deae57511474861bc6
student1 <musicsupermax@gmail.com>
2018-10-07 10:33:36 +0300

    111

---
+++ src/testGit/TestGit.java
@@ -0,0 +1,20 @@
+package testGit;
+
+/**
+ * Created by student on 07.10.2018.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        char sb = 'a';
+        String[][] st = new String[7][8];
+
+        for (int i = 0; i < st.length; i++) {
+            for (int j = 0; j < st[0].length; j++) {
+                st[i][j] = sb + "" + (j + 1);
+                System.out.print(st[i][j] + "\t");
+            }
+            sb++;
+            System.out.println("");
+        }
+    }
+}
