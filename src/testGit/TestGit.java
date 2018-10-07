9e384309bad41ecea0ad1fa9b8b30ae48de6a719
student1 <musicsupermax@gmail.com>
2018-10-07 10:35:19 +0300

    11111

--- src/testGit/TestGit.java
+++ src/testGit/TestGit.java
@@ -7,6 +7,7 @@ public class TestGit {
     public static void main(String[] args) {
         char sb = 'a';
         String[][] st = new String[7][8];
+        System.out.println("ddddddd");
 
         for (int i = 0; i < st.length; i++) {
             for (int j = 0; j < st[0].length; j++) {
---
+++ src/testGit/TestGit2.java
@@ -0,0 +1,10 @@
+package testGit;
+
+/**
+ * Created by student on 07.10.2018.
+ */
+public class TestGit2 {
+    public static void main(String[] args) {
+        System.out.println("rrrrrrrrrrrrr");
+    }
+}
